4ecec45df5f5fc2c7d5c993a099ecdf80ddc9c6a
Dele Onabowu <delesodade@hotmail.com>
2022-11-13 15:47:21 +0000

    updated code for dockerfile and docker-compose

--- site/index.php
+++ site/index.php
@@ -7,6 +7,13 @@
     <body>
         <h1>Apparel Prices</h1>
 
+    <ul>
+        
+    <?php
+        $json = file_get_contents('http://prices')
+        $price_items = json_decode($json)
 
+    ?>
+    </ul>
     </body>
 </html>
